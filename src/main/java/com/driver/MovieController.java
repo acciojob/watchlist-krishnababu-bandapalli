997777ea4533987bdc70bb65937e55ec1080bc4c
Krishnababu <krishkrishnababu@gmail.com>
2023-01-28 00:16:11 +0530

    Solution

---
+++ src/main/java/com/driver/MovieController.java
@@ -0,0 +1,60 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("movies")
+public class MovieController {
+    @Autowired
+    MovieService movieService;
+    @PostMapping("/add-movie")
+    public ResponseEntity addMovie(@RequestParam("movie") Movie movie) {
+        movieService.addMovie(movie);
+        return new ResponseEntity<>("Movie added successfully", HttpStatus.CREATED);
+    }
+    @PostMapping("/add-director")
+    public ResponseEntity addDirector(@RequestParam("director") Director director) {
+        movieService.addDirector(director);
+        return new ResponseEntity("Director added successfully",HttpStatus.CREATED);
+    }
+    @PostMapping("/add-movie-director-pair")
+    public ResponseEntity addMovieDirectorPair(@RequestParam("movie") String movieName,@RequestParam("director") String directorName) {
+        movieService.addMovieDirectorPair(movieName, directorName);
+        return new ResponseEntity("New movie-director pair added successfully",HttpStatus.CREATED);
+    }
+    @GetMapping("/get-movie-by-name/{name}")
+    public ResponseEntity getMovieByName (@PathVariable("name") String movieName) {
+        Movie movie = movieService.getMovieByName(movieName);
+        return new ResponseEntity(movie, HttpStatus.CREATED);
+    }
+    @GetMapping("/get-director-by-name/{name}")
+    public ResponseEntity getDirectorByName(@PathVariable("name") String directorName) {
+        Director director = movieService.getDirectorByName(directorName);
+        return new ResponseEntity(director, HttpStatus.CREATED);
+    }
+    @GetMapping("/get-movies-by-director-name/{director}")
+    public ResponseEntity getMoviesByDirectorName(@PathVariable("director") String directorName) {
+        List<String> movies = movieService.getMoviesByDirectorName(directorName);
+        return new ResponseEntity<>(movies, HttpStatus.CREATED);
+    }
+    @GetMapping("/get-all-movies")
+    public ResponseEntity findAllMovies() {
+        List<String> movies =  movieService.findAllMovies();
+        return new ResponseEntity(movies, HttpStatus.CREATED);
+    }
+    @DeleteMapping("/delete-director-by-name")
+    public ResponseEntity deleteDirectorByName(@RequestParam("director") String directorName) {
+        movieService.deleteDirectorByName(directorName);
+        return new ResponseEntity("Deleted director", HttpStatus.CREATED);
+    }
+    @DeleteMapping("/delete-all-directors")
+    public ResponseEntity deleteAllDirectors() {
+        movieService.deleteAllDirectors();
+        return new ResponseEntity("Deleted all movies mapped with Director", HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,68 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MovieRepository {
+    Map<String, Movie> movies;
+    Map<String, Director> directors;
+    Map<String, List<String>> moviesMappedWithDirector;
+
+    public MovieRepository() {
+        this.movies = new HashMap<>();
+        this.directors = new HashMap<>();
+        this.moviesMappedWithDirector = new HashMap<>();
+    }
+
+    public void addMovie(Movie movie) {
+        movies.put(movie.getName(), movie);
+    }
+    public void addDirector(Director director) {
+        directors.put(director.getName(), director);
+    }
+    public void addMovieDirectorPair(String movieName, String directorName) {
+        if(moviesMappedWithDirector.containsKey(directorName)) {
+            if(movies.containsKey(movieName) && directors.containsKey(directorName)) {
+                movies.put(movieName, movies.get(movieName));
+                directors.put(directorName, directors.get(directorName));
+            }
+            List<String> currentMovies;
+            currentMovies = moviesMappedWithDirector.get(directorName);
+            moviesMappedWithDirector.put(directorName, currentMovies);
+        }
+    }
+    public Movie getMovieByName (String movieName) {
+        return movies.get(movieName);
+    }
+    public Director getDirectorByName(String directorName) {
+        return directors.get(directorName);
+    }
+    public List<String> getMoviesByDirectorName(String directorName) {
+        if (moviesMappedWithDirector.containsKey(directorName)) {
+            return moviesMappedWithDirector.get(directorName);
+        }
+        return new ArrayList<>();
+    }
+    public List<String> findAllMovies() {
+        return new ArrayList<>(movies.keySet());
+    }
+    public void deleteDirectorByName(String directorName) {
+        if (moviesMappedWithDirector.containsKey(directorName)) {
+            for (String movieName: moviesMappedWithDirector.get(directorName)) {
+                movies.remove(movieName);
+            }
+            moviesMappedWithDirector.remove(directorName);
+        }
+        directors.remove(directorName);
+    }
+    public void deleteAllDirectors() {
+        for (String director: moviesMappedWithDirector.keySet()) {
+            for (String movie: moviesMappedWithDirector.get(director)) {
+                movies.remove(movie);
+            }
+        }
+        moviesMappedWithDirector.clear();
+    }
+}
